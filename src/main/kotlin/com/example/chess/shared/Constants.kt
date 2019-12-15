10d2d9ffc206c6fc0731c8727cf6c628ae0b3345
v.peschanyi <p.v.s.oren@gmail.com>
2019-12-16 00:44:18 +0300

    - working

--- src/main/kotlin/com/example/chess/shared/Constants.kt
+++ src/main/kotlin/com/example/chess/shared/Constants.kt
@@ -9,4 +9,5 @@ object Constants {
     const val BOARD_SIZE = 8
     const val ROOK_SHORT_COLUMN_INDEX = 0
     const val ROOK_LONG_COLUMN_INDEX = 7
+    const val EXPECTED_FIRST_CONSTRUCTED_HISTORY_ITEM_POSITION = 33
 }
