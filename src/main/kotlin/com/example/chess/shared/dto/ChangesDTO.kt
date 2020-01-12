81511786a32ddf8a075e046d0924161a1cddb0a6
v.peschanyi <p.v.s.oren@gmail.com>
2020-01-12 15:16:52 +0300

    - opponent changes listener

--- src/main/kotlin/com/example/chess/shared/dto/ChangesDTO.kt
+++ src/main/kotlin/com/example/chess/shared/dto/ChangesDTO.kt
@@ -1,5 +1,6 @@
 package com.example.chess.shared.dto
 
+import com.fasterxml.jackson.annotation.JsonIgnore
 import java.io.Serializable
 
 data class ChangesDTO(
@@ -7,4 +8,25 @@ data class ChangesDTO(
     val lastMove: MoveDTO,
     val additionalMove: MoveDTO?,       // в случае если произошла рокировка или взятие на проходе клиенту нужна дополнительная информация, чтобы корректно выполнить ход
     val checkedPoint: PointDTO?
-) : Serializable
+) : Serializable {
+
+    @JsonIgnore
+    fun isEmpty() = position == EMPTY_CHANGES_POSITION
+
+    companion object {
+
+        private const val EMPTY_CHANGES_POSITION = -1
+
+        @JsonIgnore
+        val EMPTY: ChangesDTO = ChangesDTO(
+            EMPTY_CHANGES_POSITION,
+            MoveDTO(
+                PointDTO(0, 0),
+                PointDTO(0, 0),
+                null
+            ),
+            null,
+            null
+        )
+    }
+}
